77317716196481834753edcfb6771bc37fc11bb1
Arthur Goudal <arthur.goudal@etu.univ-orleans.fr>
2025-03-22 16:28:28 +0100

    Page tableau des résultats d'analyse fonctionnelle

--- src/app/tableau/tableau.component.ts
+++ src/app/tableau/tableau.component.ts
@@ -1,32 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { FenetreTutoComponent } from "../fenetre-tuto/fenetre-tuto.component";
 import { LocalStorageService } from '../services/local-storage.service';
+import { ApiService } from '../services/api.service';
+import { forkJoin } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 @Component({
-  selector: 'app-tableau',
-  imports: [FenetreTutoComponent],
-  templateUrl: './tableau.component.html',
-  styleUrl: './tableau.component.scss'
+    selector: 'app-tableau',
+    imports: [CommonModule, FenetreTutoComponent],
+    templateUrl: './tableau.component.html',
+    styleUrl: './tableau.component.scss'
 })
-export class TableauComponent {
+export class TableauComponent implements OnInit{
     ongletCourant:string = 'tableau';
-    displayModal:boolean = false;
+	displayModal:boolean = false;
+	ingredientsDiscoveries: any[] = [];
+    currentIngredient: any;
 
     //TODO Message du tutoriel à modifier
-    tutorialContent:string = 'Bienvenue sur la page tableau ...';
+    tutorialContent:string = 'Bienvenue sur la page tableau. Les résultats de vos analyses sont répertoriés ici. Vous pouvez consulter les découvertes de chaque ingrédient en cliquant dessus.';
       
-    constructor(private router: Router, private localStorage: LocalStorageService) {}
+    constructor(private router: Router, private localStorage: LocalStorageService, private apiService : ApiService) {}
     
     ngOnInit() {
-      if (!this.localStorage.estDisponible("tutorielDejaVuTableau")) {
-        this.displayModal = true;
-        this.localStorage.setElement('tutorielDejaVuTableau', true);
-      }
+        const tutorielDejaVu = sessionStorage.getItem('tutorielDejaVuTableau') || 'false';
+        if (tutorielDejaVu === 'false') {
+            this.displayModal = true;
+            sessionStorage.setItem('tutorielDejaVuTableau', 'true');
+        }
+		const codeId = sessionStorage.getItem('codeId') || '';
+        const savedIngredient = sessionStorage.getItem('currentIngredient');
+        this.currentIngredient = savedIngredient ? JSON.parse(savedIngredient) : { id: -1 };
+
+        this.apiService.getIngredients(codeId).subscribe({
+            next: (ingredients: any[]) => {            
+                // Vérifier si la liste des ingrédients est vide
+                if (!ingredients || ingredients.length === 0) {
+                    console.warn('No ingredients found for codeId:', codeId);
+                    this.ingredientsDiscoveries = []; // Éviter un traitement inutile
+                    return;
+                }
+                
+                // Récupération des découvertes pour chaque ingrédient
+                ingredients.forEach((ingredient: any) => {
+                    this.apiService.getDiscoveredTable(codeId, ingredient.id).subscribe({
+                        next: (discoveredTable: any) => {
+                            // Tri des découvertes par ordre alphabétique de la condition puis par ordre croissant de la lowerBound
+                            discoveredTable.sort((a: any, b: any) => {
+                                if (a.condition < b.condition) {
+                                    return -1;
+                                }
+                                if (a.condition > b.condition) {
+                                    return 1;
+                                }
+                                return a.lowerBound - b.lowerBound;
+                            });
+                            ingredient.discoveredTable = discoveredTable;
+                        },
+                        error: (error: any) => {
+                            console.error('Error getting discovered table', error);
+                        }
+                    });
+                    this.ingredientsDiscoveries.push(ingredient);
+                });
+            },
+            error: (error: any) => {
+                console.error('Error getting ingredients', error);
+            }
+        });
     }
+
+	selectIngredient(ingredient: any) {
+		sessionStorage.setItem('currentIngredient', JSON.stringify(ingredient));
+		this.currentIngredient = ingredient;
+	}
     
     naviguerVers(page:string){
-      this.ongletCourant=page;
-      this.router.navigate([`/${page}`]);
+      	this.ongletCourant=page;
+      	this.router.navigate([`/${page}`]);
     }
 }
