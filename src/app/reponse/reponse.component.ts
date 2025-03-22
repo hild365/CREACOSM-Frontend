41ee47b61be4c1ee9bcc8296b294da056280860d
Esteban <esteban.draily@etu.univ-orleans.fr>
2025-03-22 01:18:18 +0100

    prototype de la page reponse

--- src/app/reponse/reponse.component.ts
+++ src/app/reponse/reponse.component.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FenetreTutoComponent } from '../fenetre-tuto/fenetre-tuto.component';
 import { LocalStorageService } from '../services/local-storage.service';
+import { CdkDragDrop, DragDropModule, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-reponse',
-  imports: [FenetreTutoComponent],
+  imports: [FenetreTutoComponent, DragDropModule],
   templateUrl: './reponse.component.html',
   styleUrls: ['./reponse.component.scss']
 })
 export class ReponseComponent implements OnInit {
   ongletCourant: string = 'reponse';
   displayModal: boolean = false;
-  //TODO contenu à modifier
+  items=["Beurre de karité", "Aloe Vera", "Extrait de fleurs", "Acide Citrique", "Parfum"]
+  frigo: string[]=[]
+  armoireVentilee: string[]=[]
+  armoireVitree: string[]=[]
   tutorialContent: string = 'Bienvenue sur la page réponse....';
 
   constructor(private router: Router, private localStorage: LocalStorageService) {}
@@ -23,8 +28,20 @@ export class ReponseComponent implements OnInit {
       this.localStorage.setElement('tutorielDejaVuReponse', true);
     }
   }
- 
 
+  drop(event: CdkDragDrop<string[]>){
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      // Déplacement entre les listes
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 
   naviguerVers(page: string) {
     this.ongletCourant = page;
