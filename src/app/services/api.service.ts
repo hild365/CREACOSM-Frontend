f432fe838603c1f3b1f7fc1e4873590d67c15790
Vincent Gonnet <vincent.gonnet08@gmail.com>
2025-03-26 23:55:54 +0100

    final version

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,91 +1,97 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { response } from "express";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-@Injectable
-({
-    providedIn: "root"
+@Injectable({
+  providedIn: 'root',
 })
 export class ApiService {
+  private port = environment.backendPort || 3001;
 
-    private url = `http://${window.location.hostname}:3000`;
+  private url = `http://${window.location.hostname}:${this.port}`;
 
-    private routes = {
-        getDiscoveredTable: "get-discovered-table",
-        getIngredients: "get-ingredients",
-        startGame: "start-game",
-        analyzeIngredient: "analyze-ingredient",
-        tryStorage: "try-storage",
-        getStorages: "get-storages",
-      endgame: "end-game"
-    };
+  private routes = {
+    getDiscoveredTable: 'get-discovered-table',
+    getIngredients: 'get-ingredients',
+    startGame: 'start-game',
+    analyzeIngredient: 'analyze-ingredient',
+    tryStorage: 'try-storage',
+    getStorages: 'get-storages',
+    endgame: 'end-game',
+  };
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-  
+  // ****************************************************************************************************
 
-    // ****************************************************************************************************
+  get(route: string): Observable<any> {
+    return this.http.get(`${this.url}/${route}`);
+  }
 
-    get(route: string): Observable<any> {
-        return this.http.get(`${this.url}/${route}`);
-    }
+  post(route: string, data: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    return this.http.post(`${this.url}/${route}`, data, {
+      headers,
+      responseType: 'json',
+    });
+  }
 
-    post(route: string, data: any): Observable<any> {
-        const headers = new HttpHeaders({
-            "Content-Type": "application/json",
-        });
-        return this.http.post(`${this.url}/${route}`, data, { headers, responseType: 'json' });
-    }
+  put(route: string, data: any): Observable<any> {
+    return this.http.put(`${this.url}/${route}`, data);
+  }
 
-    put(route: string, data: any): Observable<any> {
-        return this.http.put(`${this.url}/${route}`, data);
-    }
+  delete(route: string): Observable<any> {
+    return this.http.delete(`${this.url}/${route}`);
+  }
 
-    delete(route: string): Observable<any> {
-        return this.http.delete(`${this.url}/${route}`);
-    }
+  // ****************************************************************************************************
 
-    // ****************************************************************************************************
+  startGame(code: string): Observable<any> {
+    return this.post(`${this.routes.startGame}`, {
+      code,
+    });
+  }
 
-    startGame(code: string): Observable<any> {
-        return this.post(`${this.routes.startGame}`, {
-            code,
-        });
-    }
+  getDiscoveredTable(group: string, ingredientId: string): Observable<any> {
+    return this.post(`${this.routes.getDiscoveredTable}`, {
+      group,
+      ingredientId,
+    });
+  }
 
-    getDiscoveredTable(group: string, ingredientId: string): Observable<any> {
-        return this.post(`${this.routes.getDiscoveredTable}`, {
-            group,
-            ingredientId
-        });
-    }
-
-    getIngredients(group: string): Observable<any> {
-        return this.post(`${this.routes.getIngredients}`, {
-            group
-        });
-    }
-    getStorages(): Observable<any> {
-        return this.get(`${this.routes.getStorages}`);
-    }
-    analyzeIngredient(group: string, ingredientId: string,condition: string, value:string): Observable<any> {
-        return this.post(`${this.routes.analyzeIngredient}`, {
-            group,
-            ingredientId,
-            condition,
-            value
-        });
-    }
-    tryStorage(storageId: string, ingredientId: string): Observable<any> {
-        return this.post(`${this.routes.tryStorage}`, {
-            ingredientId,
-            storageId
-        });
-    }
-    endGame(group: string): Observable<any> {
-      return this.post(`${this.routes.endgame}`,{
-        group});
-    }
-
+  getIngredients(group: string): Observable<any> {
+    return this.post(`${this.routes.getIngredients}`, {
+      group,
+    });
+  }
+  getStorages(): Observable<any> {
+    return this.get(`${this.routes.getStorages}`);
+  }
+  analyzeIngredient(
+    group: string,
+    ingredientId: string,
+    condition: string,
+    value: string
+  ): Observable<any> {
+    return this.post(`${this.routes.analyzeIngredient}`, {
+      group,
+      ingredientId,
+      condition,
+      value,
+    });
+  }
+  tryStorage(storageId: string, ingredientId: string): Observable<any> {
+    return this.post(`${this.routes.tryStorage}`, {
+      ingredientId,
+      storageId,
+    });
+  }
+  endGame(group: string): Observable<any> {
+    return this.post(`${this.routes.endgame}`, {
+      group,
+    });
+  }
 }
