5ae81d1c51fdeb3f1d8496d79a4d1efb334d1ec4
Esteban <esteban.draily@etu.univ-orleans.fr>
2025-03-26 19:12:18 +0100

    bouton étape suivante

--- src/app/victoire/victoire.component.ts
+++ src/app/victoire/victoire.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { LocalStorageService } from '../services/local-storage.service';
+
+
 
 @Component({
   selector: 'app-victoire',
@@ -7,5 +11,17 @@ import { Component } from '@angular/core';
   styleUrl: './victoire.component.scss'
 })
 export class VictoireComponent {
+constructor(
+    private apiService: ApiService, private localStorage: LocalStorageService
+  ) {}
 
+  endGame(){
+    this.apiService.endGame(this.localStorage.getElement('codeId') as string).subscribe({
+      next: (data: any) => {
+      },
+      error: (error: any) => {
+        console.error('Error while ending the game:', error);
+      }
+    });
+  }
 }
